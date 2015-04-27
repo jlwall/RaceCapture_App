fb118af1dc73cd483a18e40de7d183358d955163
Brent Picasso <brent@autosportlabs.com>
2015-04-26 17:38:10 -0700

    beginnings of sampledata optimiation

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ if __name__ == '__main__' and sys.platform == 'win32':
     freeze_support()
 
 if __name__ == '__main__':
+    import pyximport; pyximport.install()
+        
     import logging
     import argparse
     import kivy
