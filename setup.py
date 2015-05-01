f5b864f3a9106fb661862ebcf39d81a9f181dc5f
Brent Picasso <brent@autosportlabs.com>
2015-05-01 15:09:37 -0700

    a minimal working setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,7 @@ from distutils.extension import Extension
 
 setup(
     name='racecaptureapp',
-    ext_modules = [Extension("blah.testit", ["blah/testit.c"], 
+    ext_modules = [Extension("autosportlabs.racecapture.data.sampledata", ["autosportlabs/racecapture/data/sampledata.c"], 
                              extra_compile_args=['-std=c99']
                               )]
 )
-
-#setup(
-#    name='racecaptureapp',
-#    ext_modules = [Extension("autosportlabs.racecapture.data.sampledata", ["autosportlabs/racecapture/data/sampledata.c"], 
-#                             extra_compile_args=['-std=c99']
-#                              )]
-#)
